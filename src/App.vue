5f44133a1b2d7cad5e5cc96e194d2b87d43352ed
Solodkih <solodkih_v_p@mail.ru>
2024-11-17 23:09:29 +0600

    test 8

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,6 @@ const currencies = ref([]);
 onMounted(async () => {
   try {
     const respons = await fetch(`${window.location.origin}/exchange-rate/data.xml`);
-    console.log(window.location.origin);
     if (respons.ok) {
       const xmlString = await respons.text();
 
